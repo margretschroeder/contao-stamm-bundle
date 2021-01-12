994bccbd50ca3cc1c54c4bfb21e75e2e717bb775
margretschroeder <margret_schroeder@freenet.de>
2021-01-12 20:56:22 +0100

    Wohnung als Backend verfügbar

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,5 @@
 <?php
 // Frontend modules
 $GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'MargretSchroeder\ContaoStammBundle\Module\HelloWorldModule';
+$GLOBALS['BE_MOD']['content']['my_new_module'] = [ 'tables' => ['tl_wohnung'],];
 ?>
